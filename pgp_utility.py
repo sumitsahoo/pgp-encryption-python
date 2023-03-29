19da8867cc67e5f2a57cb8d21d9efb0c319e0b79
Sumit Sahoo <sumitsahoo1988@gmail.com>
2023-03-29 16:18:27 +0530

    Added error handling

--- pgp_utility.py
+++ pgp_utility.py
@@ -6,7 +6,7 @@ import os
 
 gpg = gnupg.GPG()   # '/usr/local/bin/gpg' path is optional but gnupg needs to be installed
 
-# Take path and key as an input 
+# Get key and file path as input from the user
 
 symmetricKey = input("Enter password: ")
 path = input("Enter complete path to the file: ")
@@ -14,28 +14,23 @@ path = input("Enter complete path to the file: ")
 encryptedFilePath = path + '.encrypted'
 
 # Enable zlib compression when encrypting the file
-extra_args = ['--compress-algo', 'zlib']
+extra_args_encryption = ['--compress-algo', 'zlib']
 
-# Function name: encrypt_file
-# Purpose: Encrypts a file with a symmetric key
-# Parameters:
-#    path: path of the file to be encrypted
-#    symmetricKey: symmetric key to be used for encryption
-#    extra_args: extra arguments to be passed to the gpg binary
-# Return value: status of the encryption
+# Start file encryption and decryption
+try:
+    with open(path, 'rb') as file:
+        encryptionStatus = gpg.encrypt_file(file, recipients=Any, symmetric=True, passphrase=symmetricKey, output=path + ".encrypted", armor=False, extra_args=extra_args_encryption)
 
-with open(path, 'rb') as file:
-    status = gpg.encrypt_file(file, recipients=Any, symmetric=True, passphrase=symmetricKey,
-                              output=path + ".encrypted", armor=False, extra_args=extra_args)
-
-if(status.ok):
-    print("File encrypted successfully")
-
-# Decrypt
-
-with open(encryptedFilePath, 'rb') as file:
-    status = gpg.decrypt_file(
-        file, passphrase=symmetricKey, output=path + ".decrypted")
-
-if(status.ok):
-    print("File decrypted successfully")
+    if(encryptionStatus.ok):
+        print("File encrypted successfully")
+        print("Encrypted file path: " + encryptedFilePath)
+        with open(encryptedFilePath, 'rb') as file:
+            decryptionStatus = gpg.decrypt_file(file, passphrase=symmetricKey, output=path + ".decrypted")
+        if(decryptionStatus.ok):
+            print("File decrypted successfully")
+        else:
+            print("File decryption failed, Error details: " + str(decryptionStatus.status))     
+    else:
+        print("File encryption failed, Error details: " + str(encryptionStatus.status))
+except Exception as e:
+    print("Exception occurred: " + str(e))
